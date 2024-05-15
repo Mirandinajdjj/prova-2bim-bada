d8715619b03288d2df62c54a16d977706c26c21c
Mɪʀᴀɴᴅᴀ乡 <134981019+Mirandinajdjj@users.noreply.github.com>
2024-05-15 18:24:06 -0300

    prova_mbs2bim2.0

--- prova_mbs2bim.sql
+++ prova_mbs2bim.sql
@@ -2,7 +2,7 @@
 insert into Livros (titulo, autor, ano_publicacao, disponivel, categoria, isbn, editora, quantidade_paginas, idioma) values ("As Crônicas de Nárnia", "C.S Lewis", 1950, true, "Fantasia", "978-0064471190", "HaperCollins", 768, "Francês"); 
 
 # 2
-update Livros set disponivel = false where ano_publicacao >= 1980;
+update Livros set disponivel = null where ano_publicacao < 1980;
 
 # 3
 update Livros set editora = "Plume Books" where titulo = "1984";
@@ -11,7 +11,7 @@ update Livros set editora = "Plume Books" where titulo = "1984";
 delete from Livros where idioma = "Inglês" and ano_publicacao < 2000;
 
 # 5
-select * from Livros where quantidade_paginas > 650; 
+select * from Livros where quantidade_paginas > 650;
 
 # 6
 select count(categoria) from Livros group by categoria;
@@ -26,4 +26,4 @@ select avg(ano_publicacao) from Livros;
 select * from Livros order by ano_publicacao desc; 
 
 # 10
-select * from Livros WHERE Livros like '%S%' and ano_publicacao > 1974 and ano_publicacao < 1991;
+select * from Livros WHERE Livros like '%S%' and ano_publicacao BETWEEN 1975 and 1990;
